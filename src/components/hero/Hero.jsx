44be2b389173644f4f1803f0d7eba2026a3bcc9b
Anna-Bilokon <anna.bilokon@illuminum.se>
2024-08-30 08:32:59 +0200

    change titles and import pictures

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,27 +1,25 @@
 import React from 'react'
 import './hero.css'
 import arrow_icon from '../assets/arrow.png'
-import hero_image from '../assets/hero_image.png'
+import hero_image from '../assets/hero_new.svg'
 
 function Hero() {
   return (
     <div className="hero">
       <div className="hero_left">
-        <h2>NEW ARRIVALS</h2>
+        <h2>What Will You Read Next?</h2>
         <div>
           <div className="hero_hand_icon">
-            <p>new</p>
+            <p>Explore to find your new favorite book</p>
           </div>
-          <p>books</p>
-          <p>for booklovers</p>
         </div>
         <div className="hero_latest_btn">
-          Bestsellers
+          Explore
           <img src={arrow_icon} alt="" />
         </div>
       </div>
       <div className="hero_right">
-        <img src={hero_image} alt="" />
+        <img src={hero_image} alt="hero_picture" />
       </div>
     </div>
   )
