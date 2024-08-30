62017985ac7dc0f66f9b0e803b0f9122f36a49de
Anna-Bilokon <anna.bilokon@illuminum.se>
2024-08-30 08:34:23 +0200

    add the add to cart button for items

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Item.css'
 import { Link } from 'react-router-dom'
+import { ShopContext } from '../../context/ShopContext'
 
 const Item = (props) => {
+  const { product } = props
+  const { addToCart } = useContext(ShopContext)
   return (
     <div className="item">
       <Link to={`/product/${props.id}`}>
@@ -14,6 +17,14 @@ const Item = (props) => {
         <div className="item_price_new">${props.new_price}</div>
       </div>
       <div className="item_price_old">${props.old_price}</div>
+      <button
+        className="addToCardBtn"
+        onClick={() => {
+          addToCart(product.id)
+        }}
+      >
+        Add To Cart
+      </button>
     </div>
   )
 }
