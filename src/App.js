3d83463bc794ee8ffbde9f1a5d1af2575b1f90e2
Anna-Bilokon <anna.bilokon@illuminum.se>
2024-09-06 14:43:27 +0200

    added new route pathes

--- src/App.js
+++ src/App.js
@@ -10,18 +10,19 @@ import LoginSignUp from './pages/LoginSignUp';
 import banner from './components/assets/banner.png';
 import banner_1 from './components/assets/banner_1.png';
 import Footer from './components/Footer/Footer';
+import Categories from './pages/Categories';
 
 function App() {
   return (
-    <div>
+    <>
 		<BrowserRouter>
 		
       <Navbar/>
 	  <Routes>
 		<Route path='/' element={<Home/>}/>
-		<Route path='/romance' element={<ShopCategory banner={banner} category='romance'/>}/>
-		<Route path='/fantasy' element={<ShopCategory banner={banner_1} category='fantasy'/>}/>
-		<Route path='/litFiction' element={<ShopCategory banner={banner} category='literaly_fiction'/>}/>
+		<Route path='/shop' element={<ShopCategory banner={banner} category='shop'/>}/>
+		<Route path='/categories' element={<Categories banner={banner_1} category='categories'/>}/>
+		<Route path='/events' element={<ShopCategory banner={banner} category='events'/>}/>
 		<Route path='/product' element={<Product/>}> 
 		<Route path=':productId' element={<Product/>}/>
 		</Route>
@@ -31,7 +32,7 @@ function App() {
 	  </Routes>
 	  <Footer/>
 	  </BrowserRouter>
-    </div>
+    </>
   );
 }
 
